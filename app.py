f33ab1b2f70ffd84612e3ef2588acf983db871c5
vkomatip <126748175+vkomatip@users.noreply.github.com>
2023-03-14 08:38:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return 'Hello, world!'
 
 if __name__ == '__main__':
     app.run()
